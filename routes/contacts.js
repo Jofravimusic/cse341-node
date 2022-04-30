ed0d1a31b4c0fe2d147fb8bfdf12feeabba86f23
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-04-30 14:28:56 -0300

    changes made to contacts to retrieve all and one contact by id

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,11 +1,26 @@
 const routes = require('express').Router();
 const dbconnection = require("../model/dbconnection");
-
+const ObjectId = require('mongodb').ObjectId;
 routes.get('/', (req, res) => {
-    dbconnection.getContacts().find().toArray((err, result) =>{
-        if(err) throw err;
-        res.json(result);
-        console.log("Contacts being retrieved");
+    
+    let contacts = dbconnection.getContacts().find();
+
+    contacts.toArray().then((documents) =>{
+        res.status(200).json(documents);
+        console.log("All contacts were retrieved");
+    });
+    
+});
+
+routes.get('/:id', (req, res) => {
+    
+    let contactId = new ObjectId(req.params.id)
+    let contact = dbconnection.getContacts().findOne({_id : contactId});
+
+    contact.then((document) =>{
+        if(!document) return res.status(200).send(`No contact with id: ${req.params.id}`);
+        res.status(200).json(document);
+        console.log(`Contact was retrieved with id: ${req.params.id}`);
     });
     
 });
