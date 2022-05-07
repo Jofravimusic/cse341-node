cf534376c80840201edc974761dbf188a45d1650
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-05-07 19:29:26 -0300

    CRUD operations added

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,28 +1,95 @@
 const routes = require('express').Router();
-const dbconnection = require("../model/dbconnection");
-const ObjectId = require('mongodb').ObjectId;
-routes.get('/', (req, res) => {
-    
-    let contacts = dbconnection.getContacts().find();
+const { ObjectId } = require('mongodb');
 
-    contacts.toArray().then((documents) =>{
-        res.status(200).json(documents);
-        console.log("All contacts were retrieved");
-    });
-    
+const dbconnection = require('../model/dbconnection');
+
+// Get all Contacts
+routes.get('/', (req, res) => {
+  const contacts = dbconnection.getContacts().find();
+
+  contacts.toArray().then((documents) => {
+    res.status(200).json(documents);
+    console.log('All contacts were retrieved');
+  });
 });
 
-routes.get('/:id', (req, res) => {
-    
-    let contactId = new ObjectId(req.params.id)
-    let contact = dbconnection.getContacts().findOne({_id : contactId});
+// Create a Contact
+routes.post('/', (req, res) => {
+  const contact = dbconnection.getContacts().insertOne({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday,
+  });
 
-    contact.then((document) =>{
-        if(!document) return res.status(200).send(`No contact with id: ${req.params.id}`);
-        res.status(200).json(document);
-        console.log(`Contact was retrieved with id: ${req.params.id}`);
-    });
-    
+  contact.then((document) => {
+    if (!document.insertedId) return res.status(404).send(`No contact added`);
+    res.status(201).redirect(`/contacts/${document.insertedId}`);
+    console.log(`Contact was created with id: ${document.insertedId}`);
+  });
+});
+
+// Get a Contact by Id
+routes.get('/:id', (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const contact = dbconnection.getContacts().findOne({ _id: contactId });
+
+  contact.then((document) => {
+    if (!document)
+      return res.status(404).send(`No contact with id: ${req.params.id}`);
+    res.status(200).json(document);
+    console.log(`Contact was retrieved with id: ${req.params.id}`);
+  });
+});
+
+// Update a Contact by Id
+routes.put('/:id', (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const contact = dbconnection.getContacts().updateOne(
+    {
+      _id: contactId,
+    },
+    {
+      $set: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        favoriteColor: req.body.favoriteColor,
+        birthday: req.body.birthday,
+      },
+    }
+  );
+
+  contact.then((document) => {
+    if (document.matchedCount >= 1) {
+      if (document.modifiedCount < 1)
+        return res
+          .status(404)
+          .send('Contact could not be updated, nothing was changed');
+      res.status(201).json(document);
+      console.log(`Contact was updated with id: ${req.params.id}`);
+    } else {
+      res.status(404).send(`Contact was not found with id: ${req.params.id}`);
+    }
+  });
+});
+
+// Delete a Contact by Id
+routes.delete('/:id', (req, res) => {
+  const contactId = new ObjectId(req.params.id);
+  const contact = dbconnection.getContacts().deleteOne({ _id: contactId });
+
+  contact.then((document) => {
+    if (document.deletedCount < 1)
+      return res
+        .status(404)
+        .send(
+          `No contact with id: ${req.params.id} or deleted was not processed`
+        );
+    res.status(200).json(document);
+    console.log(`Contact was deleted with id: ${req.params.id}`);
+  });
 });
 
 module.exports = routes;
--- week1-server.js
+++ week1-server.js
@@ -1,13 +1,16 @@
-//Server Variables
+// Server Variables
 const express = require('express');
+
 const app = express();
 const port = process.env.PORT || 3000;
-const dbconnection = require("./model/dbconnection");
+const bodyParser = require('body-parser');
+const dbconnection = require('./model/dbconnection');
 
 dbconnection.connectDatabase();
 
-app.use('/', require('./routes'))
+app.use(bodyParser.json());
+app.use('/', require('./routes'));
 
-app.listen(port, ()=> {
-    console.log(`Running on Port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Running on Port ${port}`);
+});
