e991a1dfcc5bd4aba4b256942eed854f9594d9dd
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-05-14 15:35:00 -0300

    swagger added

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,6 @@ routes.get('/', (req, res) => {
   res.send('Hello, my dear son Francisco Aguirre');
 });
 routes.use('/contacts', require('./contacts'));
+routes.use('/api-docs', require('./doc'));
 
 module.exports = routes;
---
+++ swagger.js
@@ -0,0 +1,16 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const doc = {
+  info: {
+    title: 'Contacts API',
+    description:
+      'API for contacts for cse341 course developed by José Aguirre, following instructions from the lessons',
+  },
+  host: 'https://cse341-node-jofravimusic.herokuapp.com/',
+  schemes: ['https'],
+};
+
+const outputFile = './swagger.json';
+const endpoint = ['./routes/index.js'];
+
+swaggerAutogen(outputFile, endpoint, doc);
