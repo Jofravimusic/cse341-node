e423aa296f251b2c9af7f5831a7406c1ec55580b
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-05-14 16:25:50 -0300

    changing routes

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,21 @@ routes.get('/', (req, res) => {
 routes.use('/contacts', require('./contacts'));
 routes.use('/api-docs', require('./doc'));
 
+routes.use((req, res, next) => {
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://cse341-contacts-frontend.netlify.app'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  next();
+});
+
 module.exports = routes;
