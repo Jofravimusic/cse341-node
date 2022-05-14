6c3ef975cb1435b24ea0d00fc35cc0249962f3f9
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-05-14 09:52:11 -0300

    new app definition

--- week1-server.js
+++ week1-server.js
@@ -8,21 +8,22 @@ const dbconnection = require('./model/dbconnection');
 
 dbconnection.connectDatabase();
 
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
-  );
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, PUT, DELETE, OPTIONS'
-  );
-  next();
-});
-app.use('/', require('./routes'));
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Content-Type', 'application/json');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, OPTIONS'
+    );
+    next();
+  })
+  .use('/', require('./routes'));
 
 app.listen(port, () => {
   console.log(`Running on Port ${port}`);
