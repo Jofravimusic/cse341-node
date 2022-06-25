56d762bf2962c5b5238ef8b5a17412b620e642b7
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-06-25 11:51:54 -0300

    Added!

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router();
 
 routes.get('/', (req, res) => {
-  res.send('Hello, my dear son Francisco Aguirre');
+  res.send('Hello, my dear son Francisco Aguirre!');
 });
 routes.use('/contacts', require('./contacts'));
 routes.use('/api-docs', require('./doc'));
