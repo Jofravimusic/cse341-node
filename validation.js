2ce491516df0dce4c08857cafb092d2fe827d8a4
José Francisco Aguirre Villarroel <jofravimusic@byui.edu>
2022-06-25 22:37:49 -0300

    validation messages corrected

--- validation.js
+++ validation.js
@@ -1,13 +1,13 @@
 const { check, validationResult } = require('express-validator');
 
 exports.inputValidation = [
-  check('firstName', 'First Name is requied').not().isEmpty(),
-  check('lastName', 'Last Name is requied').not().isEmpty(),
+  check('firstName', 'First Name is required').not().isEmpty(),
+  check('lastName', 'Last Name is required').not().isEmpty(),
   check('email', 'Please include a valid email')
     .isEmail()
     .normalizeEmail({ gmail_remove_dots: true }),
-  check('favoriteColor', 'Favorite Color is requied').not().isEmpty(),
-  check('birthday', 'Birthday is requied').not().isEmpty(),
+  check('favoriteColor', 'Favorite Color is required').not().isEmpty(),
+  check('birthday', 'Birthday is required').not().isEmpty(),
 ];
 
 exports.results = validationResult;
